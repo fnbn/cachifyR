89268a036c52f28cef00b1cdd35839945716b990
markus <mbonsch@nordex-online.com>
2017-06-04 18:54:36 +0200

    Bugfix: cachify.character now assigns to .GlobalEnv to assure that the cachified function is found on the search path.

--- R/cachify.character.R
+++ R/cachify.character.R
@@ -1,14 +1,18 @@
 #' cachify turns a function into a caching function
-#' @param f name of the function to cachify
+#' @param f name of the function to cachify (character)
 #' @param debug Set to TRUE to receive info about cache retrieval vs. evaluation
 #' @param cacheDir A directory where the results of the function are stored.
 #' @return No return. The original function with name \code{f} is replaced by the cachified version
+#' @details ATTENTION: will always assign the new function to \code{.GlobalEnv}, 
+#'          no matter where the original binding of the function was.
+#'          This can be changed by setting \code{pos = pryr::where(get(f))} in the assign statement.
+#'          See http://adv-r.had.co.nz/Environments.html#function-envs.
 #' @S3method cachify character
 cachify.character <- function(f, cacheDir, debug=FALSE) {
   
   assign(x     = f, 
          value = cachify(f = get(f), cacheDir = cacheDir),
-         envir = environment(get(f)))
+         pos   = .GlobalEnv)
   
   return(invisible(NULL))
 }
