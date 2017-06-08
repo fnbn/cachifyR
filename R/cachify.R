e261177acda76936eb48a10fe5dc6c568b6cc143
markus <mbonsch@nordex-online.com>
2017-06-08 17:24:19 +0200

    Better documentation of cachify function.

--- R/cachify.R
+++ R/cachify.R
@@ -3,6 +3,8 @@
 #' @param debug Set to TRUE to receive info about cache retrieval vs. evaluation
 #' @param cacheDir A directory where the results of the function are stored.
 #' @return The cachified function. Cachifistic!
+#' @details ATTENTION: functions that are called for their side-effects become
+#'          unusable without error!
 #' @export
 cachify <- function(f, cacheDir, debug=FALSE) {
   
